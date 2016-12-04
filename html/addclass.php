7db71d7428f520d36ffd6e76afc738d33299110b
ruizry <ruizry@oregonstate.edu>
2016-12-03 18:34:40 -0800

    add class working

---
+++ html/addclass.php
@@ -0,0 +1,34 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors', 'On');
+//Connects to the database
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","ruizry-db","WeUJO0bUJKhJstCn","ruizry-db");
+if(!$mysqli || $mysqli->connect_errno){
+	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+
+if(!($stmt = $mysqli->prepare("INSERT INTO class_tbl(gid, name, classDay, classTime, durationMin, capacity) VALUES (?,?,?,?,?,?)"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!($stmt->bind_param("isssii",$_POST['GymID'],$_POST['name'],$_POST['day'],$_POST['time'],$_POST['duration'],$_POST['capacity']))){
+	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+} else {
+	echo "Class Added";
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Class Status</title>
+	</head>
+<body>
+	<div>
+		<p><a href="classData.php">Back</a></p>
+	</div>
+</body>
+</html>
