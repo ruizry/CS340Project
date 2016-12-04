4aaab4fac65ac89bc052e5665da6b60c13f7efc7
ruizry <ruizry@oregonstate.edu>
2016-12-03 18:37:19 -0800

    add header

--- html/assignemployee.php
+++ html/assignemployee.php
@@ -22,6 +22,10 @@ if(!$stmt->execute()){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
+	<head>
+		<meta charset="utf-8">
+		<title>Employee Assignment Status</title>
+	</head>
 <body>
 	<div>
 		<p><a href="employeehtml.php">Back</a></p>
