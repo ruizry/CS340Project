5a97f9ac047a041ce8cddedccf70dc40f91e8b3f
ruizry <ruizry@oregonstate.edu>
2016-12-05 15:08:54 -0800

    fixed typo

--- html/removeEmp.php
+++ html/removeEmp.php
@@ -2,8 +2,7 @@
 //Turn on error reporting
 ini_set('display_errors', 'On');
 //Connects to the database
-//$mysqli = new mysqli("oniddb.cws.oregonstate.edu","ruizry-db","WeUJO0bUJKhJstCn","ruizry-db");
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu","sanchjoh-db","pb3bG0PgvCuxtXbK","sanchjoh-db");
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","ruizry-db","WeUJO0bUJKhJstCn","ruizry-db");
 if($mysqli->connect_errno){
 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 }
@@ -19,7 +18,7 @@ if($mysqli->connect_errno){
 	<body>
 		<h1>Manage Employee Classes/Gyms</h1>
 		<div>
-			<h3>Remove Class From Member</h3>
+			<h3>Remove Class From Employee</h3>
 			<form action="deleteEmpClass.php" method="post">
 
 				<fieldset>
